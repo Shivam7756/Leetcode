d18c4ecce15d870901f9f86f2e1210efcade86cd
Shivam_1707 <56798691+Shivam7756@users.noreply.github.com>
2021-08-18 11:37:11 +0530

    Update merge-strings-alternately.java

--- merge-strings-alternately/merge-strings-alternately.java
+++ merge-strings-alternately/merge-strings-alternately.java
@@ -1,14 +1,27 @@
 class Solution {
-    public String mergeAlternately(String s1, String s2) {
-          int i=0,j=0;
-        String s="";
-        while(i<s1.length() && j<s2.length())
-           s = s + s1.charAt(i++) + s2.charAt(j++);
-        while(i<s1.length())
-            s+= s1.charAt(i++) ;
-        while(j<s2.length())
-            s+= s2.charAt(j++) ;
-        return s;
-        
+    public String mergeAlternately(String word1, String word2) {
+    char[] ch =new char[word1.length()+word2.length()];
+    int k = 0;
+    int i = 0;
+    int j = 0;
+    for(;i<word1.length() || i<word2.length();i++)
+    {
+        if(i<word1.length() && i<word2.length())
+        {
+        ch[k++]=word1.charAt(i);
+        ch[k++]=word2.charAt(j++);
+        }
+        else if(i<word1.length())
+        {
+            ch[k++]=word1.charAt(i);
+        }
+        else
+        {
+            ch[k++]=word2.charAt(j++);
+        }
+       
     }
+    
+    return new String(ch);
+}
 }
